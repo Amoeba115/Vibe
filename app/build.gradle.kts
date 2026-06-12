97614647f68dbc4bd354e4a616bef63841be63db
Ayra Hikari <AyraHikari@gmail.com>
2026-06-13 02:09:56 +0700

    Version 1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "me.ayra.music"
         minSdk = 28
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
